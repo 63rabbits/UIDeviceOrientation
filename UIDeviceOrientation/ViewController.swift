97bc1f58f665a9256c835644655c4f5e8354fc25
63rabbits <63rabbitsgoodman@gmail.com>
2020-05-14 23:50:34 +0900

    Initial Commit

---
+++ UIDeviceOrientation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDeviceOrientation
+//
+//  Created by rabbit on 2020/05/14.
+//  Copyright © 2020 rabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
